3d18f8bd841c0374684d8af1ac2b7dd214fb86d6
Django-B <django.ppp@gmail.com>
2024-07-28 12:44:55 +0300

    10.3: Работа с ListView, вывод списка статей

--- blog_cbv/apps/blog/views.py
+++ blog_cbv/apps/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post 
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['title'] = 'Главная страница'
+        return context

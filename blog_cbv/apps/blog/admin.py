540962b365ba0333e021bb88f4953252820916f0
Django-B <django.ppp@gmail.com>
2024-08-04 21:54:44 +0300

    11.1: Создание древовидных комментириев

--- blog_cbv/apps/blog/admin.py
+++ blog_cbv/apps/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 
 @admin.register(Category)
@@ -11,3 +11,9 @@ class CategoryAdmin(DjangoMpttAdmin):
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
+@admin.register(Comment)
+class CommentAdminPage(DjangoMpttAdmin):
+    """
+    Админ-панель модели комментариев
+    """
+    pass
